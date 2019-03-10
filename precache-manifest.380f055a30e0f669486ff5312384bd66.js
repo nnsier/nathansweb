a87ad38829c0fc95188da58769c1f21c236d022e
ecthyr <arreisnahtan@gmail.com>
2019-03-09 22:28:22 -0500

    Updates

---
+++ precache-manifest.380f055a30e0f669486ff5312384bd66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c4bbbf8e3ebc9fc8fe7",
+    "url": "/nathansweb/static/js/runtime~main.17c41f25.js"
+  },
+  {
+    "revision": "d26644da50edc2c059d7",
+    "url": "/nathansweb/static/js/main.f00fb8d8.chunk.js"
+  },
+  {
+    "revision": "2408024a7a15c2332c2e",
+    "url": "/nathansweb/static/js/2.e3f89015.chunk.js"
+  },
+  {
+    "revision": "d26644da50edc2c059d7",
+    "url": "/nathansweb/static/css/main.a26ae993.chunk.css"
+  },
+  {
+    "revision": "ee2a3d9d040a377f7499f864846c229f",
+    "url": "/nathansweb/index.html"
+  }
+];
