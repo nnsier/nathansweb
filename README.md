fb6cd1e3d5f894a32870814355ddc3bfa56c1f8b
ecthyr <arreisnahtan@gmail.com>
2019-04-04 15:58:33 -0400

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-
-<h2>Nathan Sierra's Website</h2>
-
-<p>This website's front-end is done with React. I'm still messing around with the landing page, and trying to decide what to do with it.</p>
---
+++ precache-manifest.9c2a1ed574470c3431aa9d020856d54c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "da2110ccbc4d48446032",
+    "url": "/static/js/main.59e06ab1.chunk.js"
+  },
+  {
+    "revision": "2408024a7a15c2332c2e",
+    "url": "/static/js/2.e3f89015.chunk.js"
+  },
+  {
+    "revision": "da2110ccbc4d48446032",
+    "url": "/static/css/main.4e39dbe9.chunk.css"
+  },
+  {
+    "revision": "5c85539fb1c8ac8a84a37d0d66311274",
+    "url": "/index.html"
+  }
+];
